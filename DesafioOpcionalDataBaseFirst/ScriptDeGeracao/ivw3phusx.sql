af233d0b19f4a6df32f22839b21db1e8de050620
FelipeN21 <felipenepomuceno21@hotmail.com>
2022-02-27 15:09:57 -0300

    Script de criação

---
+++ DesafioOpcionalDataBaseFirst/ScriptDeGeracao/ivw3phusx.sql
@@ -0,0 +1,18 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+CREATE TABLE [Cliente] (
+    visit_id INT PRIMARY KEY IDENTITY (1, 1),
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    visited_at DATETIME,
+    phone VARCHAR(20),
+    store_id INT NOT NULL
+);
